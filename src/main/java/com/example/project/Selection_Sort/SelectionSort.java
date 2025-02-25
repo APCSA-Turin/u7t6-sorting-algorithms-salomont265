9b7fad98c10ed7f0c83d7c557e261e07a8870637
Salomon Tartakovskiy <saltart2180@gmail.com>
2025-02-25 16:42:51 +0000

    push

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,14 +8,42 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-       
+
+       for(int i=0;i<elements.length;i++){
+        int smallIndex = i;
+        int smallest = Integer.MAX_VALUE;
+        for(int j= i;j<elements.length;j++){
+            if(elements[j] < smallest){
+                smallIndex = j;
+                smallest = elements[j];
+            }
+        }
+        //swap
+        int temp = elements[i];
+        elements[i] = smallest;
+        elements[smallIndex] = temp;
+       }
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-       
+       for(int i=0;i<words.size();i++){
+        int smallIndex = i;
+        String smallest = "z";
+        for(int j= i;j<words.size();j++){
+            if(words.get(j).compareTo(smallest) < 0){
+                smallIndex = j;
+                smallest = words.get(j);
+            }
+        }
+        //swap
+        String temp = words.get(i);
+        words.set(i, smallest);
+        words.set(smallIndex,temp);
+       }
+      
         return words;
     }
 
